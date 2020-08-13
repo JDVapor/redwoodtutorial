4a4690e35e75c53b55e01c8c040611d22f306908
JDVapor <jdvapordestiny@gmail.com>
2020-08-12 18:40:28 -0700

    add auth

--- redwoodblog/web/src/layouts/BlogLayout/BlogLayout.js
+++ redwoodblog/web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,26 +1,34 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
-  return (
-    <>
-      <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
 
-            <li>
-              <Link to={routes.contact()}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
+
+  return (
+    <div>
+      <h1>
+        <Link to={routes.home()}>Redwood Blog</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+          <li>
+            <Link to={routes.contact()}>Contact</Link>
+          </li>
+          <li>
+            <a href="#" onClick={isAuthenticated ? logOut : logIn}>
+              { isAuthenticated ? 'Log Out' : 'Log In' }
+            </a>
+          </li>
+
+          { isAuthenticated && <li>{currentUser.email}</li> }
+        </ul>
+      </nav>
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
